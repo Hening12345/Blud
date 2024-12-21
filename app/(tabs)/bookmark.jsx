081136f790d45bd3fbd8d9207cde200f0b5683a7
Hening12345 <hening.rifqi@gmail.com>
2024-12-21 15:38:30 +0700

    1

---
+++ app/(tabs)/bookmark.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import "../../global.css";
+
+const Bookmark = () => {
+  return (
+    <View>
+      <Text>Bookmark</Text>
+    </View>
+  )
+}
+
+export default Bookmark
---
+++ components/SearchInput.jsx
@@ -0,0 +1,49 @@
+import { View, Text, TextInput, TouchableOpacity, Image, Alert } from 'react-native';
+import React, { useState } from 'react';
+import "../global.css";
+import { icons } from '../constants';
+import { router, usePathname } from 'expo-router';
+
+const SearchInput = ({ initialQuery }) => {
+  const pathname = usePathname()
+  const [query, setQuery] = useState(initialQuery || '')
+  const [isFocused, setIsFocused] = useState(false); 
+
+  return (
+    <View
+        className={`w-full h-16 px-4 bg-black-100 rounded-2xl flex-row items-center space-x-4`}
+        style={{
+        borderWidth: 2,
+        borderColor: isFocused ? '#FFA001' : '#7b7b8b'
+        }}>
+        <TextInput
+            className="text-base mt-1 text-white flex-1 font-pregular"
+            value={query}
+            placeholder="Search for a video topic"
+            placeholderTextColor="#CDCDE0"
+            onChangeText={(e) => setQuery(e)}
+            onFocus={() => setIsFocused(true)} 
+            onBlur={() => setIsFocused(false)} 
+        />
+
+        <TouchableOpacity
+          onPress={() => {
+            if(!query) {
+              return Alert.alert('Missing query', 'Please input something to search')
+            }
+
+            if(pathname.startsWith('/search')) router.setParams({query})
+              else router.push(`/search/${query}`)
+          }}
+        >
+            <Image
+            source={icons.search}
+            className="w-5 h-5"
+            resizeMode="contain"
+            />
+        </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchInput
